f134688ec8db63e9aa75b7b3846559aa0e99e0a5
KoenHaemels <koenhaemels@gmail.com>
2017-04-27 11:07:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ cd /var
 sudo git clone https://github.com/KoenHaemels/InstallationApplicationGcloud.git
 cd InstallationApplicationGcloud/
 echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
-/bin/sed -i "s|accountFILE|${HOME}/gcloud-service-key.json|g" Credits/template.json
-/bin/sed -i "s/projectID/"${PROJECT_ID_GCLOUD}"/g" Credits/template.json
+sudo /bin/sed -i "s|accountFILE|${HOME}/gcloud-service-key.json|g" Credits/template.json
+sudo /bin/sed -i "s/projectID/"${PROJECT_ID_GCLOUD}"/g" Credits/template.json
 ./main.sh
