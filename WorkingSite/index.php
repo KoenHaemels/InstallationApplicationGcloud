df9cbb30a6a925f88b7ac301f03c317a5b57f836
KoenHaemels <koenhaemels@gmail.com>
2017-05-09 11:53:52 +0200

    Update index.php

--- WorkingSite/index.php
+++ WorkingSite/index.php
@@ -28,18 +28,20 @@
 </form>
 <div class="well">
 	<h2><u>Files bucket:</u></h2>
-	<?php
-	$path = "/var/InstallationApplicationAWS/bucket/";
-	$dh = opendir($path);
-	$i=1;
-	while (($file = readdir($dh)) !== false) {
-	if($file != "." && $file != "..") {
-        	echo "<br /> <p>$file</p>";
-        	$i++;
-    		}
-	}
-closedir($dh);
+<?php
+ if ($handle = opendir('/var/InstallationApplicationGcloud/bucket')) {
+   while (false !== ($file = readdir($handle)))
+      {
+          if ($file != "." && $file != "..")
+          {
+                $thelist .= '<p>'.$file.'</p>';
+          }
+       }
+  closedir($handle);
+  }       
 ?>
+<P>List of files:</p>
+<P><?=$thelist?></p>
 </div>
 </div>
     </body>
